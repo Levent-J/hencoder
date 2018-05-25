3bbe4c6cd1924bc0e1a816ab9dd7e12e1e478ec5
liwenjing <levent_j@163.com>
2018-05-25 10:22:31 +0800

    p1-10

--- PracticeDraw1/app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.java
+++ PracticeDraw1/app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.java
@@ -2,6 +2,9 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -26,5 +29,35 @@ public class Practice10HistogramView extends View {
 
 //        综合练习
 //        练习内容：使用各种 Canvas.drawXXX() 方法画直方图
+
+        Paint mPaint = new Paint();
+        mPaint.setStrokeWidth(5);
+
+        canvas.translate(200,600);
+
+        Path path = new Path();
+        path.moveTo(0,-400);
+        path.lineTo(0,0);
+        path.lineTo(800,0);
+
+
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setColor(Color.WHITE);
+        canvas.drawPath(path,mPaint);
+
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setColor(Color.GREEN);
+        canvas.drawRect(50,-10,150,0,mPaint);
+        canvas.drawRect(200,-200,300,0,mPaint);
+        canvas.drawRect(350,-400,450,0,mPaint);
+        canvas.drawRect(500,-150,600,0,mPaint);
+
+        mPaint.setColor(Color.WHITE);
+        mPaint.setTextSize(40);
+        canvas.drawText("aaa",50,50,mPaint);
+        canvas.drawText("bbb",200,50,mPaint);
+        canvas.drawText("ccc",350,50,mPaint);
+        canvas.drawText("ddd",500,50,mPaint);
+
     }
 }
