0b6e19e1f979a1b475abf66fea46f3e95d93ca9f
liwenjing <levent_j@163.com>
2018-05-25 10:41:51 +0800

    p1 fin

--- PracticeDraw1/app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice11PieChartView.java
+++ PracticeDraw1/app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice11PieChartView.java
@@ -2,6 +2,9 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -26,5 +29,28 @@ public class Practice11PieChartView extends View {
 
 //        综合练习
 //        练习内容：使用各种 Canvas.drawXXX() 方法画饼图
+
+        Paint mPaint = new Paint();
+
+        canvas.translate(getWidth()/2,getHeight()/2);
+        RectF rectF = new RectF(-400,-400,400,400);
+
+        mPaint.setColor(Color.BLUE);
+        canvas.drawArc(rectF,0,20,true,mPaint);
+
+        mPaint.setColor(Color.GREEN);
+        canvas.drawArc(rectF,20,20,true,mPaint);
+
+        mPaint.setColor(Color.RED);
+        canvas.drawArc(rectF,40,45,true,mPaint);
+
+        mPaint.setColor(Color.parseColor("#10c659"));
+        canvas.drawArc(rectF,85,125,true,mPaint);
+
+        mPaint.setColor(Color.parseColor("#ea7425"));
+        canvas.drawArc(rectF,210,100,true,mPaint);
+
+        mPaint.setColor(Color.YELLOW);
+        canvas.drawArc(rectF,310,50,true,mPaint);
     }
 }
