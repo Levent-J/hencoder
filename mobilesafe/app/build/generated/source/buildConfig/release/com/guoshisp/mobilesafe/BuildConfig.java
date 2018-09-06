f964714c6d918f5442645723a94cc9ebf34c0aab
liwenjing <levent_j@163.com>
2018-09-06 10:48:24 +0800

    111

---
+++ mobilesafe/app/build/generated/source/buildConfig/release/com/guoshisp/mobilesafe/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.guoshisp.mobilesafe;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.guoshisp.mobilesafe";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
